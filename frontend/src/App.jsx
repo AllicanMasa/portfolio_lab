032db9a736b5b84e9f86262a4492369b0360b984
Allican Masa <masa.karlallican.02252002@gmail.com>
2026-02-04 22:55:22 +0800

    initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+// import Login from "./login/login";
+import Register from "./register/register";
+
+function App() {
+  return (
+    <div>
+      <Register/>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/src/login/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>HAHAHAHHAHAHA</div>
+  )
+}
+
+export default login
